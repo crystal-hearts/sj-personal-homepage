18bf0d4dbcd66cb050533f74543dbee9ea824650
Sarah <sarahjacques@bitbucket.org>
2024-11-30 20:15:04 -0800

    SJ-12 Adjustments to styling

--- app/page.tsx
+++ app/page.tsx
@@ -82,6 +82,29 @@ function Footer() {
   );
 }
 
+function FlavorButton({ flavor }: { flavor: string }) {
+  const buttonClasses =
+    "flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--" +
+    flavor +
+    "-500)] border-opacity-80 bg-[var(--" +
+    flavor +
+    "-500)] px-2 py-1 text-[var(--" +
+    flavor +
+    "-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105";
+
+  return (
+    <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
+      <button
+        className={buttonClasses}
+        onClick={() => changeThemeColor(flavor)}
+      />
+      <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
+        {flavor}
+      </span>
+    </div>
+  );
+}
+
 function TextBubbles() {
   return (
     <div className="text-bubbles flex flex-row text-sm font-black text-[var(--theme-color-700)]">
@@ -94,6 +117,43 @@ function TextBubbles() {
       <button className="text-bubble mr-2 rounded border-1 border-solid border-[var(--theme-color-500)] border-opacity-80 bg-[var(--theme-color-100)] px-2 py-1 text-center shadow-[rgba(0,0,0.5,0.5)_5px_5px_5px_0px] transition duration-150 hover:scale-110 active:scale-105">
         about my cat
       </button>
+    </div>
+  );
+}
+
+function SettingsModal() {
+  return (
+    <div
+      id="settings-modal"
+      className="settings-modal fixed left-4 top-full z-20 overflow-hidden rounded border-1 border-solid border-[var(--theme-color-500)] border-opacity-80 bg-[var(--theme-color-100)] shadow-[rgba(0,0,0.5,0.5)_5px_5px_5px_0px] transition-transform duration-500 modal-open:-translate-y-full"
+      data-modal-active="closed"
+    >
+      <div className="modal-contents">
+        <div className="modal-header flex flex-row items-center justify-between border-b-2 border-solid border-[var(--theme-color-500)] border-opacity-80 bg-[var(--theme-color-700)] py-1 pl-4 pr-3">
+          <h2
+            className="modal-heading select-none text-sm font-bold text-[var(--theme-color-100)]"
+            tabIndex="-1"
+          >
+            site flavor
+          </h2>
+          <button
+            className="modal-close h-4 w-4 fill-[var(--theme-color-100)]"
+            onClick={() => closeModal("settings-modal", "settings-button")}
+          >
+            <CloseIcon />
+          </button>
+        </div>
+        <div className="modal-contents h-full px-1 py-2">
+          <div className="flavor-buttons flex flex-row flex-wrap">
+            <FlavorButton flavor="ube" />
+            <FlavorButton flavor="raspberry" />
+            <FlavorButton flavor="strawberry" />
+            <FlavorButton flavor="lemon" />
+            <FlavorButton flavor="lime" />
+            <FlavorButton flavor="mint" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -199,86 +259,7 @@ export default function Home() {
             </h1>
           </div>
         </div>
-        <div
-          id="settings-modal"
-          className="settings-modal fixed left-4 top-full z-20 overflow-hidden rounded border-1 border-solid border-[var(--theme-color-500)] border-opacity-80 bg-[var(--theme-color-100)] shadow-[rgba(0,0,0.5,0.5)_5px_5px_5px_0px] transition-transform duration-500 modal-open:-translate-y-full"
-          data-modal-active="closed"
-        >
-          <div className="modal-contents">
-            <div className="modal-header flex flex-row items-center justify-between border-b-2 border-solid border-[var(--theme-color-500)] border-opacity-80 bg-[var(--theme-color-700)] py-1 pl-4 pr-3">
-              <h2
-                className="modal-heading select-none text-sm font-bold text-[var(--theme-color-100)]"
-                tabIndex="-1"
-              >
-                site flavor
-              </h2>
-              <button
-                className="modal-close h-4 w-4 fill-[var(--theme-color-100)]"
-                onClick={() => closeModal("settings-modal", "settings-button")}
-              >
-                <CloseIcon />
-              </button>
-            </div>
-            <div className="modal-contents h-full px-1 py-2">
-              <div className="flavor-buttons flex flex-row flex-wrap">
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--ube-500)] border-opacity-80 bg-[var(--ube-500)] px-2 py-1 text-[var(--ube-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("ube")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    ube
-                  </span>
-                </div>
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--raspberry-500)] border-opacity-80 bg-[var(--raspberry-500)] px-2 py-1 text-[var(--raspberry-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("raspberry")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    raspberry
-                  </span>
-                </div>
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--strawberry-500)] border-opacity-80 bg-[var(--strawberry-500)] px-2 py-1 text-[var(--strawberry-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("strawberry")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    strawberry
-                  </span>
-                </div>
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--lemon-500)] border-opacity-80 bg-[var(--lemon-500)] px-2 py-1 text-[var(--lemon-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("lemon")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    lemon
-                  </span>
-                </div>
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--lime-500)] border-opacity-80 bg-[var(--lime-500)] px-2 py-1 text-[var(--lime-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("lime")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    lime
-                  </span>
-                </div>
-                <div className="flavor-button-container mb-2 mr-4 flex min-w-14 flex-col items-center">
-                  <button
-                    className="flavor-button mb-1 h-8 w-8 rounded border-1 border-solid border-[var(--mint-500)] border-opacity-80 bg-[var(--mint-500)] px-2 py-1 text-[var(--mint-900)] shadow-[rgba(0,0,0.5,0.5)_3px_3px_3px_0px] transition duration-150 hover:scale-105 active:scale-105"
-                    onClick={() => changeThemeColor("mint")}
-                  />
-                  <span className="flavor-button-label ml-1 w-full text-center text-xs font-semibold">
-                    mint
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SettingsModal />
       </main>
       <Footer />
     </div>
