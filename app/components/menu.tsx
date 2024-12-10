b8a80c21d0a35322588181db8cd0e1a266043d09
Sarah <sarahjacques@bitbucket.org>
2024-12-09 19:57:35 -0800

    SJ-9 SJ-10 SJ-11 - Removing Home button from menu

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -6,9 +6,6 @@ import { MenuItem } from "@/components/menu-item";
 export function Menu() {
   return (
     <div className="menu flex flex-row space-x-2 text-sm font-black text-[var(--theme-color-700)]">
-      <MenuItem id="home-button" title={SITE_CONSTANTS.HOME_TITLE} href="/">
-        {SITE_CONSTANTS.HOME_TITLE}
-      </MenuItem>
       <MenuItem
         id="about-me-button"
         title={SITE_CONSTANTS.ABOUT_ME_TITLE}
