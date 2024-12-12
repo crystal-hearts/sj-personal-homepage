ec5324d295599958893c042ced8c1f2862a2f66c
Sarah <sarahjacques@bitbucket.org>
2024-12-12 12:19:48 -0800

    SJ-16 Accessibility fixes

--- app/components/fixed-footer.tsx
+++ app/components/fixed-footer.tsx
@@ -7,7 +7,10 @@ export function FixedFooter({ children }: { children?: React.ReactNode }) {
       className="fixed-footer fixed right-0 z-10 h-20 md:bottom-0 md:left-0"
     >
       <div className="footer-container">
-        <nav className="footer-items flex flex-row justify-between px-6 py-6">
+        <nav
+          className="footer-items flex flex-row justify-between px-6 py-6"
+          aria-label="Site controls"
+        >
           {children}
         </nav>
       </div>
